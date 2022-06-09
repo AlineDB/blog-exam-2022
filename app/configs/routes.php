966aa5ca0a951f3b0dd7854323f87bcd45911090
aline <aline.debarros@student.hepl.be>
2022-06-09 10:52:07 +0200

    init 3

---
+++ app/configs/routes.php
@@ -0,0 +1,68 @@
+<?php
+return [
+    [
+        'method' => 'POST',
+        'action' => 'store',
+        'resource' => 'post',
+        'controller' => 'PostController',
+        'callback' => 'store',
+    ],
+    [
+        'method' => 'GET',
+        'action' => 'show',
+        'resource' => 'post',
+        'controller' => 'PostController',
+        'callback' => 'show',
+    ],
+    [
+        'method' => 'GET',
+        'action' => 'index',
+        'resource' => 'post',
+        'controller' => 'PostController',
+        'callback' => 'index',
+    ],
+    [
+        'method' => 'GET',
+        'action' => 'create',
+        'resource' => 'post',
+        'controller' => 'PostController',
+        'callback' => 'create',
+    ],
+    [
+        'method' => 'GET',
+        'action' => 'login',
+        'resource' => 'auth',
+        'controller' => 'SessionController',
+        'callback' => 'create',
+    ],
+    [
+        'method' => 'POST',
+        'action' => 'login',
+        'resource' => 'auth',
+        'controller' => 'SessionController',
+        'callback' => 'store',
+    ],
+    [
+        'method' => 'POST',
+        'action' => 'logout',
+        'resource' => 'auth',
+        'controller' => 'SessionController',
+        'callback' => 'destroy',
+    ],
+
+    [
+        'method'=>'GET',
+        'action'=>'show',
+        'resource'=>'rss',
+        'controller'=>'PostController',
+        'callback'=>'rss',
+    ],
+    [
+        'method'=>'POST',
+        'action'=>'profile',
+        'resource'=>'auth',
+        'controller'=>'SessionController',
+        'callback'=>'modifier'
+    ]
+
+];
